7157abd3b6ae14b1eb7c5c313b108937dbe3f8a9
Kimi <mrgaonju@gmail.com>
2019-07-16 23:46:08 +0800

    removeElement#27

---
+++ 27.remove-element.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=27 lang=javascript
+ *
+ * [27] Remove Element
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+};
+
